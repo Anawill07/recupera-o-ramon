85251bc7cc5485e2d67c33470352a713365d95e3
ANA CAROLINA WILL <ana_carolina-will@estudante.sesisenai.org.br>
2025-04-24 08:12:36 -0300

    novos métodos

--- recuperacao.js
+++ recuperacao.js
@@ -7,13 +7,28 @@ class Personagem{
 }
 
 class Guerreiro extends Personagem{
+ constructor(atacar, defender, vigiar){
+    this.atacar = atacar
+    this.defender = defender
+    this.vigiar = vigiar
 
+ }
 }
 
 class Mago extends Personagem{
+    constructor(atacar, defender, usarMagia){
+        this.atacar = atacar
+        this.defender = defender
+        this.usarMagia = usarMagia
     
+ }
 }
 
 class Arqueiro extends Personagem{
+    constructor(atacar, defender, usarPoder){
+        this.atacar = atacar
+        this.defender = defender
+        this.usarPoder = usarPoder
     
+    }
 }
